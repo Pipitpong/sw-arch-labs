f0bc998a3e1b75b829c48ef08abc9a4bedf7f573
Pipitpong <pipitpong.pi@gmail.com>
2022-03-24 18:45:58 +0700

    book done

---
+++ lab7/book/BookReader.java
@@ -0,0 +1,17 @@
+package book;
+
+
+import java.util.List;
+
+public class BookReader {
+    public static void main(String[] args) {
+        Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        printToScreen(book);
+    }
+    public static void printToScreen(Book book) {
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
+    }
+}
+
